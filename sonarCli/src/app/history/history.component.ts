beaab03fd33bb183419f8b9ace38d8bd6e144ef0
rafamotao <motarafa@hotmail.com>
2017-06-12 11:46:04 +0100

    fix some initial errors

--- sonarCli/src/app/history/history.component.ts
+++ sonarCli/src/app/history/history.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HistoryComponent implements OnInit {
 
    public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Sensor1'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Sensor2'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Sensor3'}
   ];  
     public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
     public lineChartOptions:any = {
