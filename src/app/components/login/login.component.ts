a85e45d3688108b47d625ee9d60427dabaf8057e
Rui Freitas <rui4825@gmail.com>
2017-06-22 22:29:59 +0100

    login email

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -61,8 +61,7 @@ export class LoginComponent implements OnInit {
   }
    validateEmail() {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        //return re.test(this.model.email);
-        return true;
+        return re.test(this.model.email);
     }
 
 }
